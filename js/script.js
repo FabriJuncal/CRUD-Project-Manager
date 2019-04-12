c021ada1fbc5b6693c6142a3495725efa19e974b
Fabricio Juncal <juncalfabri@gmail.com>
2019-04-12 02:01:39 -0300

    Se dio funcionalidad para Crear Nuevos Proyectos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 
 eventListeners();
 
+// Lista de Proyectos
+var listaProyectos = document.querySelector('ul#proyectos');
+
 function eventListeners(){
     //Boton para Crer Proyectos
     document.querySelector('.crear-proyecto a').addEventListener('click', nuevoProyecto);
@@ -8,5 +11,31 @@ function eventListeners(){
 
 function nuevoProyecto(e){
     e.preventDefault();
-    console.log('Presionaste en nuevo Proyecto');
+    console.log('Presionaste Nuevo Proyecto');
+            
+
+    // Creamos un <input> para el nombre  del nuevo proyecto
+    var nuevoProyecto = document.createElement('li');
+    nuevoProyecto.innerHTML = '<input type="text" id="nuevo-proyecto">';
+    listaProyectos.appendChild(nuevoProyecto);
+    
+    // Seleccionamos el ID con el nuevoProyecto
+    var inputNuevoProyecto = document.querySelector('#nuevo-proyecto');
+    
+    // Al presionar la tecla ENTER se crea un Nuevo Proyecto
+    inputNuevoProyecto.addEventListener('keypress', function(e){
+        
+       
+        var tecla = e.which || e.keycode;
+        if(tecla === 13){
+            guardarProyectoBD(inputNuevoProyecto.value);
+            listaProyectos.removeChild(nuevoProyecto);
+            
+        }
+        
+    })
+}
+
+function guardarProyectoBD(nombreProyecto){
+    console.log(nombreProyecto);
 }
